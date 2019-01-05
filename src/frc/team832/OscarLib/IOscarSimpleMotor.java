90acf601a238a52d64d513c57d4844df515756c9
Oscar <rhsoscar@gmail.com>
2019-01-04 23:45:32 -0500

    Initial commit

---
+++ src/frc/team832/OscarLib/IOscarSimpleMotor.java
@@ -0,0 +1,10 @@
+package frc.team832.OscarLib;
+
+public interface IOscarSimpleMotor {
+
+    /**
+     * Sets motor power
+     * @param power value from -1.0 to 1.0
+     */
+    public void set(double power);
+}
---
+++ src/frc/team832/OscarLib/OscarCANTalon.java
@@ -0,0 +1,17 @@
+package frc.team832.OscarLib;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class OscarCANTalon implements IOscarSimpleMotor {
+
+    private TalonSRX _talon;
+
+    public OscarCANTalon(int canId) {
+        _talon = new TalonSRX(canId);
+    }
+
+    @Override
+    public void set(double power) {
+
+    }
+}
