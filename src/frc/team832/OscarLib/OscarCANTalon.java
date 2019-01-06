645af6f5b2fb4de1ead849c6a73bf989f816c590
Banks T <btrout.dhrs@gmail.com>
2019-01-05 20:18:31 -0500

    Added required method and local objects for IOscarSmartMotor implementation in OscarCANTalon, fixed set method in OscarCanTalon

--- src/frc/team832/OscarLib/OscarCANTalon.java
+++ src/frc/team832/OscarLib/OscarCANTalon.java
@@ -1,17 +1,25 @@
 package frc.team832.OscarLib;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class OscarCANTalon implements IOscarSmartMotor {
 
     private TalonSRX _talon;
+    private ControlMode _ctrlMode;
 
     public OscarCANTalon(int canId) {
         _talon = new TalonSRX(canId);
+        _ctrlMode = ControlMode.Disabled;
     }
 
     @Override
     public void set(double power) {
-        _talon.set();
+        _talon.set(_ctrlMode, power);
+    }
+
+    @Override
+    public void setMode(ControlMode mode) {
+        _ctrlMode = mode;
     }
 }
