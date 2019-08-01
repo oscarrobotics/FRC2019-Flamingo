62f1e388e183611268583323775486c72ecc0f36
Banks Troutman <btrout.dhrs@gmail.com>
2019-08-01 01:18:56 -0400

    Removed old folders

--- src/main/java/frc/team832/robot/Commands/HatchFunctions/TopRocketHatch.java
+++
@@ -1,9 +0,0 @@
-package frc.team832.robot.Commands.HatchFunctions;
-
-import edu.wpi.first.wpilibj.command.CommandGroup;
-
-public class TopRocketHatch extends CommandGroup {
-	public TopRocketHatch(){
-		// TODO: DO !
-	}
-}
