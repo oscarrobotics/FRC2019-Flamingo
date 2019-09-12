6fadd2e68bc7e98cee01f629081ba8e116d6289a
Gavin Arnett <gmanarnett@gmail.com>
2019-09-12 17:42:02 -0400

    9/12 Work on StratComInterface - add buttons

--- src/main/java/frc/team832/robot/RobotContainer.java
+++ src/main/java/frc/team832/robot/RobotContainer.java
@@ -10,10 +10,14 @@ import frc.team832.robot.subsystems.Intake;
 public class RobotContainer {
 
     public static final XboxController drivePad = new XboxController(0);
-    public static final JoystickButton aButton = new JoystickButton(drivePad, 1);
-    public static final JoystickButton bButton = new JoystickButton(drivePad, 2);
-    public static final JoystickButton yButton = new JoystickButton(drivePad, 4);
-    public static final JoystickButton xButton = new JoystickButton(drivePad, 3);
+//    public static final JoystickButton aButton = new JoystickButton(drivePad, 1);
+//    public static final JoystickButton bButton = new JoystickButton(drivePad, 2);
+//    public static final JoystickButton yButton = new JoystickButton(drivePad, 4);
+//    public static final JoystickButton xButton = new JoystickButton(drivePad, 3);
+
+    public static final StratComInterface stratComInterface = new StratComInterface(1);
+    public static final JoystickButton arcadeBlackLeft = new JoystickButton(stratComInterface, 12);
+    public static final JoystickButton arcadeWhiteLeft = new JoystickButton(stratComInterface, 14);
 
     public static boolean init() {
         boolean successful = true;
@@ -31,10 +35,15 @@ public class RobotContainer {
         }
 
         // Commands
-        aButton.whenHeld(new CargoDown());
-        yButton.whenHeld(new CargoUp());
-        bButton.whenHeld(new HatchIn());
-        xButton.whenHeld(new HatchOut());
+//        aButton.whenHeld(new CargoDown());
+//        yButton.whenHeld(new CargoUp());
+//        bButton.whenHeld(new HatchIn());
+//        xButton.whenHeld(new HatchOut());
+        arcadeBlackLeft.whenHeld(new CargoUp());
+        arcadeWhiteLeft.whenHeld(new CargoDown());
+
+
+
 
 
         return successful;
--- src/main/java/frc/team832/robot/StratComInterface.java
+++ src/main/java/frc/team832/robot/StratComInterface.java
@@ -1,6 +1,7 @@
 package frc.team832.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class StratComInterface extends GenericHID {
 
@@ -8,7 +9,7 @@ public class StratComInterface extends GenericHID {
 		super(port);
 	}
 
-	public enum SCIButton {
+	public enum SCIButton { //Represent buttons on StratComInterface
 		SC1(1),
 		SC2(2),
 		SC3(3),
@@ -34,31 +35,84 @@ public class StratComInterface extends GenericHID {
 		SCIButton(int value) {
 			this.value = value;
 		}
-
 	}
 
+//	private static JoystickButton SC1 = new JoystickButton(this, SCIButton.SC1.value);
+
+	private JoystickButton blackL = new JoystickButton(this, SCIButton.ArcadeBlackLeft.value);
+	private JoystickButton blackR = new JoystickButton(this, SCIButton.ArcadeBlackRight.value);
+	private JoystickButton whiteL = new JoystickButton(this, SCIButton.ArcadeWhiteLeft.value);
+	private JoystickButton whiteR = new JoystickButton(this, SCIButton.ArcadeWhiteRight.value);
+
+	//Gets current state of key switch
 	public boolean getKeySwitch() { return getRawButton(SCIButton.KeySwitch.value);}
-
 	public boolean getKeySwitchDisabled() { return getRawButtonReleased(SCIButton.KeySwitch.value); }
-
 	public boolean getKeySwitchEnabled() { return getRawButtonPressed(SCIButton.KeySwitch.value); }
 
-	public double getXAxis(){
-		return getRawAxis(0);
-	}
+	//Gets current states for each of the main SCI buttons (the ones with the numbers)
+	public boolean getSC1() { return getRawButton(SCIButton.SC1.value);}
+		public boolean getSC1Pressed() { return getRawButtonPressed(SCIButton.SC1.value);}
+		public boolean getSC1Released() { return getRawButtonReleased(SCIButton.SC1.value);}
+	public boolean getSC2() { return getRawButton(SCIButton.SC2.value);}
+		public boolean getSC2Pressed() { return getRawButtonPressed(SCIButton.SC2.value);}
+		public boolean getSC2Released() { return getRawButtonReleased(SCIButton.SC2.value);}
+	public boolean getSC3() { return getRawButton(SCIButton.SC3.value);}
+		public boolean getSC3Pressed() { return getRawButtonPressed(SCIButton.SC3.value);}
+		public boolean getSC3Released() { return getRawButtonReleased(SCIButton.SC3.value);}
+	public boolean getSC4() { return getRawButton(SCIButton.SC4.value);}
+		public boolean getSC4Pressed() { return getRawButtonPressed(SCIButton.SC4.value);}
+		public boolean getSC4Released() { return getRawButtonReleased(SCIButton.SC4.value);}
+	public boolean getSC5() { return getRawButton(SCIButton.SC5.value);}
+		public boolean getSC5Pressed() { return getRawButtonPressed(SCIButton.SC5.value);}
+		public boolean getSC5Released() { return getRawButtonReleased(SCIButton.SC5.value);}
+	public boolean getSC6() { return getRawButton(SCIButton.SC6.value);}
+		public boolean getSC6Pressed() { return getRawButtonPressed(SCIButton.SC6.value);}
+		public boolean getSC6Released() { return getRawButtonReleased(SCIButton.SC6.value);}
 
-	public double getYAxis(){
-		return getRawAxis(1);
-	}
+	//Gets current state of plus and minus buttons
+	public boolean getSCPlus() { return getRawButton(SCIButton.SCPlus.value);}
+		public boolean getSCPlusPressed() { return getRawButtonPressed(SCIButton.SCPlus.value);}
+		public boolean getSCPlusReleased() { return getRawButtonReleased(SCIButton.SCPlus.value);}
+	public boolean getSCMinus() { return getRawButton(SCIButton.SCMinus.value);}
+		public boolean getSCMinusPressed() { return getRawButtonPressed(SCIButton.SCPlus.value);}
+		public boolean getSCMinusReleased() { return getRawButtonReleased(SCIButton.SCPlus.value);}
 
+	//Gets current state of SCI side buttons
+	public boolean getSCSideTop() { return getRawButton(SCIButton.SCSideTop.value);}
+		public boolean getSCSideTopPressed() { return getRawButtonPressed(SCIButton.SCSideTop.value);}
+		public boolean getSCSideTopReleased() { return getRawButtonReleased(SCIButton.SCSideTop.value);}
+	public boolean getSCSideMid() { return getRawButton(SCIButton.SCSideMid.value);}
+		public boolean getSCSideMidPressed() { return getRawButtonPressed(SCIButton.SCSideMid.value);}
+		public boolean getSCSideMidReleased() { return getRawButtonReleased(SCIButton.SCSideMid.value);}
+	public boolean getSCSideBot() { return getRawButton(SCIButton.SCSideBot.value);}
+		public boolean getSCSideBotPressed() { return getRawButtonPressed(SCIButton.SCSideBot.value);}
+		public boolean getSCSideBotReleased() { return getRawButtonReleased(SCIButton.SCSideBot.value);}
+
+	//Gets current state of the 4 arcade buttons
+	public boolean getArcadeBlackLeft() { return getRawButton(SCIButton.ArcadeBlackLeft.value);}
+		public boolean getArcadeBlackLeftPressed() { return getRawButtonPressed(SCIButton.ArcadeBlackLeft.value);}
+		public boolean getArcadeBlackLeftReleased() { return getRawButtonReleased(SCIButton.ArcadeBlackLeft.value);}
+	public boolean getArcadeBlackRight() { return getRawButton(SCIButton.ArcadeBlackRight.value);}
+		public boolean getArcadeBlackRightPressed() { return getRawButtonPressed(SCIButton.ArcadeBlackRight.value);}
+		public boolean getArcadeBlackRightReleased() { return getRawButtonReleased(SCIButton.ArcadeBlackRight.value);}
+	public boolean getArcadeWhiteLeft() { return getRawButton(SCIButton.ArcadeWhiteLeft.value);}
+		public boolean getArcadeWhiteLeftPressed() { return getRawButtonPressed(SCIButton.ArcadeWhiteLeft.value);}
+		public boolean getArcadeWhiteLeftReleased() { return getRawButtonReleased(SCIButton.ArcadeWhiteLeft.value);}
+	public boolean getArcadeWhiteRight() { return getRawButton(SCIButton.ArcadeWhiteRight.value);}
+		public boolean getArcadeWhiteRightPressed() { return getRawButtonPressed(SCIButton.ArcadeWhiteRight.value);}
+		public boolean getArcadeWhiteRightReleased() { return getRawButtonReleased(SCIButton.ArcadeWhiteRight.value);}
+
+	//Gets state of single toggle
+	public boolean getSingleToggle() { return getRawButton(SCIButton.SingleToggle.value);}
+
+	//Gets state of double toggle
+	public boolean getDoubleToggleUp() { return getRawButton(SCIButton.DoubleToggleUp.value);}
+	public boolean getDoubleToggleDown() { return getRawButton(SCIButton.DoubleToggleDown.value);}
+
+	//Left slider
 	@Override
-	public double getX(Hand hand) {
-		return getX();
-	}
-
+	public double getX(Hand hand) { return getX();}
+	//Right slider
 	@Override
-	public double getY(Hand hand) {
-		return getY();
-	}
-
+	public double getY(Hand hand) { return getY();}
 }
